21eaa36856ddbfb687179d414c832258b3e50cfb
Alejandro Kinsbrunner <kinsbrunner@gmail.com>
2018-08-27 08:32:20 -0300

    Introducing Input handling and preparing auth action to be hooked once added the HTTP call

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -8,74 +8,98 @@ import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import PlaceInput from '../../components/PlaceInput/PlaceInput';
 import PickImage from '../../components/PickImage/PickImage';
 import PickLocation from '../../components/PickLocation/PickLocation';
+import validate from '../../utility/validation';
 
 class SharePlaceScreen extends Component {
-    static navigatorStyle = {
-        navBarButtonColor: "orange"
-    }
+  static navigatorStyle = {
+    navBarButtonColor: "orange"
+  }
 
-    state = {
-        placeName: ""
-    }
-
-    constructor (props) {
-        super(props);
-        this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
-    }
-
-    onNavigatorEvent = event => {
-        if (event.type === "NavBarButtonPress") {
-            if (event.id === "sideDrawerToggle") {
-                this.props.navigator.toggleDrawer({
-                    side: "left"
-                });
-            }
+  state = {
+    controls: {
+      placeName: {
+        value: "",
+        valid: false,
+        touched: false,
+        validationRules: {
+          notEmpty: true
         }
+      }
     }
+  }
 
-    placeNameChangedHandler = val => {
-        this.setState({
-            placeName: val
-        })
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
+  onNavigatorEvent = event => {
+    if (event.type === "NavBarButtonPress") {
+      if (event.id === "sideDrawerToggle") {
+        this.props.navigator.toggleDrawer({
+          side: "left"
+        });
+      }
     }
+  }
 
-    placeAddedHandler = () => {
-        if (this.state.placeName.trim() !== "") {
-            this.props.onAddPlace(this.state.placeName);
+  placeNameChangedHandler = val => {
+    this.setState(prevState => {
+      return {
+        controls: {
+          ...prevState.controls,
+          placeName: {
+            ...prevState.controls.placeName,
+            value: val,
+            valid: validate(val, prevState.controls.placeName.validationRules),
+            touched: true
+          }
         }
-    }
+      };
+    });
+  }
 
-    render () {
-        return (
-            <ScrollView>
-                <View style={styles.container}>
-                    <MainText>
-                        <HeadingText>Share a place with us!</HeadingText>
-                    </MainText>
-                    <PickImage />
-                    <PickLocation />
-                    <PlaceInput 
-                        placeName={this.state.placeName}
-                        onChangeText={this.placeNameChangedHandler}
-                    />
-                    <Button title="Share the place!" onPress={this.placeAddedHandler} />
-                </View>
-            </ScrollView>
-        );
+  placeAddedHandler = () => {
+    if (this.state.controls.placeName.value.trim() !== "") {
+      this.props.onAddPlace(this.state.controls.placeName.value);
     }
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Share a place with us!</HeadingText>
+          </MainText>
+          <PickImage />
+          <PickLocation />
+          <PlaceInput
+            placeData={this.state.controls.placeName}
+            onChangeText={this.placeNameChangedHandler}
+          />
+          <Button
+            title="Share the place!"
+            onPress={this.placeAddedHandler}
+            disabled={!this.state.controls.placeName.valid}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        alignItems: "center"
-    }
+  container: {
+    flex: 1,
+    alignItems: "center"
+  }
 });
 
 mapDispatchToProps = dispatch => {
-    return {
-        onAddPlace: (placeName) => dispatch(addPlace(placeName)) 
-    };
+  return {
+    onAddPlace: (placeName) => dispatch(addPlace(placeName))
+  };
 };
 
 export default connect(null, mapDispatchToProps)(SharePlaceScreen);
